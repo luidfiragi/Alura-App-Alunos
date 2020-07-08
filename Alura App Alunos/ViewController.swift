93631dfdb094ab4701ce873f1a281363b657b299
luidfiragi <34800161+luidfiragi@users.noreply.github.com>
2020-07-08 11:23:56 -0300

    Initial Commit

---
+++ Alura App Alunos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alura App Alunos
+//
+//  Created by Emerson Firagi on 08/07/20.
+//  Copyright © 2020 Emerson Firagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
